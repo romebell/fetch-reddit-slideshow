7491d525370475f57a61c43947740bc3325b7609
margaret-jihua <margaret.jihua@gmail.com>
2020-07-22 22:13:41 -0700

    [js] fetch to the thumbnail in reddit

--- js/app.js
+++ js/app.js
@@ -8,4 +8,16 @@ submit.addEventListener('click', function() {
     console.log(input);
 })
 
+fetch('https://www.reddit.com/search.json?q=cats+nsfw:no')
+.then(response => {
+    return response.json()
+})
+.then(jsonData => {
+    let data = jsonData.data
+    let children = data.children
+    let eachChild = children[1]
+    let childData = eachChild.data
+    let picture = childData.thumbnail
+    console.log(picture);
 
+})
