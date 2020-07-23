59c531550c77311c59d981082f08394e0c60430a
Nicholas Phillips <nicholasphillips@Nicholass-MacBook-Air.local>
2020-07-23 00:25:26 -0700

    add for loop for images

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const form = document.querySelector('.container')
+const holderImage = document.querySelector(".holder")
 form.addEventListener("submit", function(e){
     e.preventDefault()
     const value = document.getElementById("input").value
@@ -7,11 +8,24 @@ form.addEventListener("submit", function(e){
     console.log(url)
     let response = fetch(url).then(function(response){
         return response.json()
+        
+        
+        
     }).then(function(data){
+        let thumbSearch = data.data.children;
         console.log(data)
+        
+        for (let i = 0; 1 < 9; i++){
+            
+            let  = thumbnails = thumbSearch[1].data.thumnail;
+            console.log(thumbnails);
+        }
+        
     })
+
     console.log(response)
 })
+
 
 
 
