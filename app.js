a6278ef19d416b67f84f04fca7c3b8e4c924b7b0
General Assembly <generalassembly@Davids-MacBook-Pro.local>
2020-07-22 20:34:36 -0700

    [html] Add search input and button

---
+++ app.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+const requestURL = "https://www.reddit.com/search.json?q=" + /*search input*/ + "+nsfw:no"
+
+
+const container = document.quesrySelector(".container")
+
+
+
+
+
+
+
+
+
+
+
+}
