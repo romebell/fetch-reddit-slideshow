6717e6dd49eb2762f8bef8647abf28ac42b081db
Michaudsr <68039781+Michaudsr@users.noreply.github.com>
2020-07-23 00:18:11 -0400

    [js] add eventlistener

--- js/app.js
+++ js/app.js
@@ -1,3 +1,24 @@
-document.addEventListener('DOMContentLoaded', function(){
-    const requestURL="http://www.reddit.com/search.json?q=cats+nsfw:no"
-    
+
+    const json ="http://www.reddit.com/search.json?q=cats+nsfw:no"
+    const button = document.querySelector('button');
+    const input = document.querySelector('input');
+
+
+    document.addEventListener('DOMContentLoaded', function(){
+        json.addEventListener('click', function() {
+            
+        })
+
+
+
+
+
+
+
+
+
+fetch("http://www.reddit.com/search.json?q=cats+nsfw:no")
+.then(function(json){
+
+
+}
