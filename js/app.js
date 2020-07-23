ec5cb9393985b51ced9293ec107cbfadbfbf0a1d
Barent Langwell <barent@Barents-MacBook-Pro.local>
2020-07-22 21:16:48 -0700

    [slideshow] Parse json data

--- js/app.js
+++ js/app.js
@@ -1,15 +1,20 @@
 const submitButton = document.querySelector('button')
 const textField = document.querySelector('input')
 const redditJSON = 'https://www.reddit.com/search.json?q='
-
+const sfw = '+nsfw:no'
 
 document.addEventListener('DOMContentLoaded', function() {
     submitButton.addEventListener('click', function() {
         console.log(textField.value);
-        fetch(redditJSON + textField.value)
-        .then(function(responseData) {
+        fetch(redditJSON + textField.value + sfw)
+        .then(function(responseData) { // fetch data with url
             // let userSearch = redditJSON + textField.value;
             console.log(responseData);
+            return responseData.json(); // return in json for that
+        })
+        .then(function(jsonData) {
+            let results = jsonData;
+            console.log(results);
             
         })
         
