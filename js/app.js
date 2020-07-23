ca14e2cbacacaca17db1c88460def5d1c7112d6d
margaret-jihua <margaret.jihua@gmail.com>
2020-07-22 21:46:06 -0700

    Add EventListener click, input value pop up in console log but doesn't stay

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-console.log("Working");
+const body = document.querySelector('body')
+const form = document.querySelector('.search-bar')
+const submit = document.querySelector('.search-btn')
+
+submit.addEventListener('click', function() {
+    console.log("searching");
+    const input = document.querySelector('.search-input').value
+    console.log(input);
+})
+
+
