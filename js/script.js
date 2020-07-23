22092de5eb3a612c7dc93f224d546ddc74ba6c1e
Elaine <elaine@Felipes-iMac.local>
2020-07-23 12:39:22 -0500

    [script] add image source and carousel

--- js/script.js
+++ js/script.js
@@ -1,57 +1,96 @@
 
-//starts with an event listener, activated by the submit button.
-//event listener logs the value of entry into searchTerm, updates requestURL
-//then it activates fetch to create an array of thumbnails
-//it uses these thumbnail values to create the carousel function
-//it will also allow for a button that will make slideshow stop
+// //starts with an event listener, activated by the submit button.
+// //event listener logs the value of entry into searchTerm, updates requestURL
+// //then it activates fetch to create an array of thumbnails
+// //it uses these thumbnail values to create the carousel function
+// //it will also allow for a button that will make slideshow stop
 
-//function to pull element from form and log into entry
-//let input = 'kitten';
+// //function to pull element from form and log into entry
+// //let input = 'kitten';
 
 
-let button = document.getElementsByTagName("submit");
-function changeHTML(){
-    var searchTerm= document.getElementById("input").value;
-    let requestURL = 'https://www.reddit.com/search.json?q=' +searchTerm;
-    console.log(requestURL);
-}
-document.addEventListener("click", changeHTML);
-    //can't seem to get event listener to work on button, just sampling with document until later
-//isn't pulling from the site
+// let button = document.getElementsByTagName("submit");
+// function changeHTML(){
+//     var searchTerm= document.getElementById("input").value;
+//     let requestURL = 'https://www.reddit.com/search.json?q=' +searchTerm;
+//     console.log(requestURL);
+// }
+// document.addEventListener("click", changeHTML);
+//     //can't seem to get event listener to work on button, just sampling with document until later
+// //isn't pulling from the site
 
-    function makeCarousel(){
-        const carouselDiv = document.createElement('div');
-        carouselDiv.classList.add('carousel-item');
-        const image = document.createElement('img');
-        image.classList.add('d-block w-100');
-    }
-        fetch(requestURL)
-        .then(function(responseData){
-            return responseData.json()
-        })
-.then(jsonData =>{
-    for(i = 0; i<jsonData.length; i++){
-    let pictures = jsonData.data.children[i].date.thumbnail;
-    console.log(pictures);
-    pictures.forEach(makeCarousel)}
+//     function makeCarousel(){
+//         const carouselDiv = document.createElement('div');
+//         carouselDiv.classList.add('carousel-item');
+//         const image = document.createElement('img');
+//         image.classList.add('d-block w-100');
+//     }
+//         fetch(requestURL)
+//         .then(response => {
+//             return response.json();
+//         }
+// .then(data =>{
+//     for(i = 0; i<50; i++) {
+//     let pictures = data.data.children[i].date.thumbnail;
+//     console.log(pictures);
+//     pictures.forEach(makeCarousel);
+//     };
 
-console.log(jsonData);
-});
+
+// }))
   
-//pictures found in data.children.data.thumbnail
+// //pictures found in data.children.data.thumbnail
 
 
-//next add carousel from bootstrap to appear on click. Also create button to clear event listeners.
-//   //<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
-//   <div class="carousel-inner">
-//   <div class="carousel-item active">
-//     <img class="d-block w-100" src="..." alt="First slide">
-//   </div>
-//   <div class="carousel-item">
-//     <img class="d-block w-100" src="..." alt="Second slide">
-//   </div>
-//   <div class="carousel-item">
-//     <img class="d-block w-100" src="..." alt="Third slide">
-//   </div>
-// </div>
-// </div>
+// //next add carousel from bootstrap to appear on click. Also create button to clear event listeners.
+// //   //<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+// //   <div class="carousel-inner">
+// //   <div class="carousel-item active">
+// //     <img class="d-block w-100" src="..." alt="First slide">
+// //   </div>
+// //   <div class="carousel-item">
+// //     <img class="d-block w-100" src="..." alt="Second slide">
+// //   </div>
+// //   <div class="carousel-item">
+// //     <img class="d-block w-100" src="..." alt="Third slide">
+// //   </div>
+// // </div>
+// // </div>
+
+var container1 = document.getElementById('container1'); 
+var container2 = document.getElementById('container2');
+var image1 = document.getElementById('image1'); 
+var stop1 = document.getElementById('stop1');
+var firstPart = 'https://www.reddit.com/search.json?q=';
+var lastPart = '+nsfw:no';
+var submit1 = document.getElementById('submit1');
+document.getElementById('submit1').addEventListener('click', submitOne);
+document.getElementById('stop1').addEventListener('click', stopOne);
+function submitOne() {
+    container1.style.display='none';
+    container2.style.display='block';
+    var weSearch = document.getElementById('search1').value;
+    var fullAddress = firstPart + weSearch + lastPart;
+    fetch(fullAddress)
+    .then (response => {
+        return response.json();
+    })
+    .then(data => {
+        console.log(data.data);
+        console.log(fullAddress);
+        console.log(data.data.children[3].data.thumbnail);
+        var numb1 = 1;
+        imgRot();
+        function imgRot() {
+            image1.src = data.data.children[numb1].data.thumbnail;
+            console.log(data.data.children[numb1].data.thumbnail);
+            numb1 ++;
+            setTimeout(imgRot, 2000);
+            }
+        })
+    }
+//function stopOne () {
+    //container1.style.display='block';
+    //container2.style.display='none';
+//};
+    
