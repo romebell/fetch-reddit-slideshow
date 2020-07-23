702321ae5b738e6a271bca3e7bac52372dc47aa2
Barent Langwell <barent@Barents-MacBook-Pro.local>
2020-07-22 21:01:48 -0700

    [slideshow] Add fetch function

--- js/app.js
+++ js/app.js
@@ -1,9 +1,22 @@
 const submitButton = document.querySelector('button')
 const textField = document.querySelector('input')
+const redditJSON = 'https://www.reddit.com/search.json?q='
 
 
 document.addEventListener('DOMContentLoaded', function() {
     submitButton.addEventListener('click', function() {
         console.log(textField.value);
+        fetch(redditJSON + textField.value)
+        .then(function(responseData) {
+            // let userSearch = redditJSON + textField.value;
+            console.log(responseData);
+            
+        })
+        
+        
     })
 })
+
+// fetch reddit search results
+
+
