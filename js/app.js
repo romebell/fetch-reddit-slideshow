a30509a8ecc6696be02a079900b1110bc6ab1509
iwasnevergivenaname <hannah.mace93@gmail.com>
2020-07-22 21:54:23 -0700

    add boiler plate and some design layout before implementing api

---
+++ js/app.js
@@ -0,0 +1,14 @@
+const body = document.querySelector(".body");
+
+fetch("https://www.reddit.com/search.json?q=shakira+nsfw:no")
+    .then(res => {
+        return res.json();
+    }).then(data => {
+        console.log(data);
+})
+
+//
+// data
+// children
+// data
+// url_overridden_by_dest
