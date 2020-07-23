5be60908af754825ef5b99a8d5d746bf91c2a6b5
Alice Aekyung Huh <aliceaekyunglee@alices-imac.myfiosgateway.com>
2020-07-23 14:10:29 -0400

    [reddit slide show] Ad function "

--- app.js
+++ app.js
@@ -1,23 +1,53 @@
 document.addEventListener('DOMContentLoaded', function(){
-    let requestUrl= url
-    let basicUrl = "https://reddit.com/search.json?q="
-    let userInput = ""    
-    let url = basicUrl + userInput + '+nsfw:no'
+    
+    
+    
+    let slide = document.createElement('div')
+    let ledditURL = `https://reddit.com/search.json?q=`
+    let userSearch = '' 
+    let nsfw = '+nsfw:no'
+    let requestURL = ledditURL + userSearch + nsfw
+    console.log(requestURL);
+
+    //select the button element by its id and add a click eventlisetner 
+    //which runs a function
+    //inside of the function select the element  I want to remove by its
+    //unique identifier 
+    //set the display style of this element to none
 
 //fetch
-    fetch(requestURL)
+    fetch(requestURL,{mode:'no-cors'})
     .then(response => {
-        return response.json()
-        console.log(response)
+        console.log(response);
+        return response.json();
+        
+        
+    })
+    .catch(error => {
+        console.log('This is an error: ' + error);
     })
     .then(data => {
         console.log(data);
-        let result = []
-        for(i = 0; i < data.children.length ; i++);
-        let images = data.children.data[i].thumnail;
-        result.push(images);
+        let slide = []
+        // for(i = 0; i < data.children.length ; i++){
+        // let images = data.children[i].data.thumbnail;
+        // slide.push(images);
+        
+
+        // gallery = document.getElementsByClassName('container')
+        // for (i = 0; i < slide.length ; i++)
+        // galleryImage = slide[i]
+        // }
+        
 
     })
+
+
+    function textInput() {
+    var userInput = document.getElementsByClassName('form-control').value
+       
+    }
+    
 })
 
 
