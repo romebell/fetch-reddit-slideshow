bde1d2d4cd186854bca8ffc6a80d8a89722d1ddf
Irene <airine@Irenes-MacBook-Air.local>
2020-07-23 14:53:52 -0700

    [constructor]Add Caar function

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const requestURL = "https://www.reddit.com/search.json?q=new_car_launching+nsfw:no"
+    const imageArray = document.querySelectorAll(".carousel-item")
+
 
     fetch(requestURL)
     .then (function(response) {
@@ -8,10 +10,25 @@ document.addEventListener('DOMContentLoaded', function() {
     })
 
     .then(data => {
-        console.log(data);
+        console.log(data.data.children[0].data.thumbnail);
 
-        // const image = data.data.children.data
-        // console.log(image);
+        let thumbSearch = data.data.children;     // declare and make a function to thumbnail
+        for (let i = 0; i < thumbSearch.length; i++) {
+            let thumbNail = thumbSearch[i].data.thumbnail;
+            console.log(thumbNail)
+
+            imageArray[i].src = thumbNail;
+
+
+        }
+
+        
+
+
+    })
+    .catch (error => {
+        console.log("It's error", error);
     })
 
 })
+
