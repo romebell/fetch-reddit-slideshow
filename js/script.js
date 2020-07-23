3ee51f50ea8b66c177fee9ca45769810c581bc7b
Elaine <elaine@Felipes-iMac.local>
2020-07-23 13:05:29 -0500

    [script] adds stopOne, some corrections

--- js/script.js
+++ js/script.js
@@ -66,31 +66,31 @@ var lastPart = '+nsfw:no';
 var submit1 = document.getElementById('submit1');
 document.getElementById('submit1').addEventListener('click', submitOne);
 document.getElementById('stop1').addEventListener('click', stopOne);
+
 function submitOne() {
     container1.style.display='none';
     container2.style.display='block';
     var weSearch = document.getElementById('search1').value;
     var fullAddress = firstPart + weSearch + lastPart;
     fetch(fullAddress)
-    .then (response => {
-        return response.json();
-    })
-    .then(data => {
-        console.log(data.data);
-        console.log(fullAddress);
-        console.log(data.data.children[3].data.thumbnail);
-        var numb1 = 1;
-        imgRot();
-        function imgRot() {
-            image1.src = data.data.children[numb1].data.thumbnail;
-            console.log(data.data.children[numb1].data.thumbnail);
-            numb1 ++;
-            setTimeout(imgRot, 2000);
+        .then (response => {
+            return response.json();
+        })
+        .then(data => {
+            var numb1 = 1;
+            imgRot();
+            function imgRot() {
+                image1.src = data.data.children[numb1].data.thumbnail;
+                console.log(data.data.children[numb1].data.thumbnail);
+                numb1 ++;
+                setTimeout(imgRot, 2000);
             }
         })
-    }
-//function stopOne () {
-    //container1.style.display='block';
-    //container2.style.display='none';
-//};
+}
+
+    function stopOne () {
+    container1.style.display='block';
+    container2.style.display='none';
+    clearTimeout(submitOne(imgRot));
+    };
     
