14ac4bb6d9cbfc7a176b0bcd9494555e93b278f6
margaret-jihua <margaret.jihua@gmail.com>
2020-07-23 11:08:23 -0700

    [js] Add div, have problem with line 15

--- js/app.js
+++ js/app.js
@@ -1,6 +1,22 @@
 const body = document.querySelector('body')
 const searchBar = document.querySelector('.search-bar')
 const submit = document.querySelector('.search-btn')
+let slidesCount = 0
+
+function showSlides() {    
+    let hiddenSlides = document.querySelectorAll('.each-slide')
+    for (let i = 0; i < hiddenSlides.length; i++){
+        hiddenSlides[i].style.display = 'none';
+    }
+    slidesCount ++
+    if (slidesCount > hiddenSlides.length) {
+        slidesCount = 1
+    }
+    hiddenSlides[slidesCount-1].style.display = 'block';
+    console.log(slideIndex);
+    console.log(slidesCount);
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
 
 submit.addEventListener('click', function() {
     searchBar.style.display = 'none'
@@ -23,38 +39,68 @@ submit.addEventListener('click', function() {
                 pictureUrls.push(pictureUrl)
             }        
         })
-        
-        // Add Carousel from Bootstrap
+
+        // Add slideshow div
 
         const carousel = document.createElement('div')
-        carousel.setAttribute('id', 'carouselExampleSlidesOnly')
-        carousel.setAttribute('class', 'carousel slide')
-        carousel.setAttribute('data-ride', 'carousel')
+        carousel.classList.add('carousel')
 
-        const carouselInner = document.createElement('div')
-        carouselInner.classList.add('carousel-inner')
-        
-        for ( let i = 0; i < pictureUrls.length; i++) {
-            const carouselItem = document.createElement('div')
-            carouselItem.classList.add('carousel-item')
-            if (i === 0) {
-                carouselItem.classList.add('active')
-            }
+        const slides = document.createElement('div')
+        slides.classList.add('slides')
 
+        for (let i = 0; i < pictureUrls.length; i++) {
+
+            let url = pictureUrls[i]
+
+            const eachSlide = document.createElement('div')
+            eachSlide.classList.add('each-slide')
+                
             const image = document.createElement('img')
-            image.src = pictureUrls[i]
-            image.setAttribute('class', 'd-block w-100')
-            
-            carouselItem.appendChild(image)
-            carouselInner.appendChild(carouselItem)
-            
+            image.src = url
+            image.classList.add('pictures')
+
+            eachSlide.appendChild(image)
+            slides.appendChild(eachSlide)
+
         }
-        console.log(carouselInner);
-        carousel.appendChild(carouselInner)
-        body.appendChild(carousel)
-        console.log(body);
+        
+        showSlides()
+
+        carousel.appendChild(slides)
+        body.appendChild(carousel)       
     })
 })
+
+ 
+        // // Add Carousel from Bootstrap
+
+        // const carousel = document.createElement('div')
+        // carousel.setAttribute('id', 'carouselExampleSlidesOnly')
+        // carousel.setAttribute('class', 'carousel slide')
+        // carousel.setAttribute('data-ride', 'carousel')
+
+        // const carouselInner = document.createElement('div')
+        // carouselInner.classList.add('carousel-inner')
+        
+        // for ( let i = 0; i < pictureUrls.length; i++) {
+        //     const carouselItem = document.createElement('div')
+        //     carouselItem.classList.add('carousel-item')
+        //     if (i === 0) {
+        //         carouselItem.classList.add('active')
+        //     }
+
+        //     const image = document.createElement('img')
+        //     image.src = pictureUrls[i]
+        //     image.setAttribute('class', 'd-block w-100')
+            
+        //     carouselItem.appendChild(image)
+        //     carouselInner.appendChild(carouselItem)
+            
+        // }
+        // console.log(carouselInner);
+        // carousel.appendChild(carouselInner)
+        // body.appendChild(carousel)
+        // console.log(body);
 
 //     <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
 //   <div class="carousel-inner">
