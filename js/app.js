9c6735c4ec185a37eb009278079863f4aaec5268
Barent Langwell <barent@Barents-MacBook-Pro.local>
2020-07-23 10:05:46 -0700

    [slideshow] Refactor code, Add js

--- js/app.js
+++ js/app.js
@@ -1,41 +1,46 @@
-const submitButton = document.querySelector('button')
-const textField = document.querySelector('input')
-const container = document.querySelector('.container')
+const submitButton = document.querySelector('#search-button');
+const textField = document.querySelector('input');
+const container = document.querySelector('.container');
 const header = document.querySelector('header');
 const redditJSON = 'https://www.reddit.com/search.json?q='
 const sfw = '+nsfw:no'
+// let imageArray = 
 
 document.addEventListener('DOMContentLoaded', function() {
     submitButton.addEventListener('click', function() {
-        console.log(textField.value);
-        fetch(redditJSON + textField.value + sfw)
-        .then(function(responseData) { // fetch data with url
-            // let userSearch = redditJSON + textField.value;
-            console.log(responseData);
+      console.log(textField.value);
+      fetch(redditJSON + textField.value + sfw)
+      .then(function(responseData) { // fetch data with url
+          // let userSearch = redditJSON + textField.value;
+          console.log(responseData);
+          if (responseData.status === 200) { // still getting self errors
             return responseData.json(); // return in json for that
-        })
-        .then(function(jsonData) {
-            let results = jsonData;
-            let children = results.data.children
-            console.log(children);
-            for (let i = 0; i <= 10; i++) {
-                // does a catch() call go in here?
-                let newImage = document.createElement('img');
-                newImage.src = children[i].data.thumbnail;
-                container.appendChild(newImage);
+          }
+      })
+      .then(function(jsonData) {
+          let results = jsonData;
+          let children = results.data.children
+          console.log(children);
+          for (let i = 0; i <= 10; i++) {
+              // does a catch() call go in here?
+              let newImage = document.createElement('img');
+              newImage.src = children[i].data.thumbnail;
+              container.appendChild(newImage);
+              // make an array instead? 
+              // imageArray.push children[i].data.thumbnail;
+          }
 
-            }
-
-        })
-        hideElements();
-        
-    })
+      })
+      hideElements();
+      
+  })
 })
 
 function hideElements() {
   submitButton.style.visibility = 'hidden';
   textField.style.visibility = 'hidden';
   header.style.visibility = 'hidden';
+  showStopButton();
 }
 
 function showElements() {
@@ -44,6 +49,12 @@ function showElements() {
   header.style.visibility = 'visible';
 }
 
+function showStopButton() {
+  const stopButton = document.createElement('button');
+  stopButton.setAttribute('id', 'stop-button');
+  stopButton.textContent = 'Stop Slideshow  '
+  container.appendChild(stopButton);
+}
 
 
 const carouselControls = document.createElement('div');
@@ -64,7 +75,12 @@ activeImage.classList.add('d-block', 'w-100');
 activeImage.setAttribute('alt', 'First slide');
 // add newImage.src at [0] activeImage.src =
 
-// const carouselNotActive = ''
+// append to carousel inner
+const carouselItemNotActive = document.createElement('div');
+carouselItemNotActive.classList.add('d-block', 'w-100');
+carouselItemNotActive.setAttribute('alt', 'Second slide');
+
+
 
 // carouselControls.
 // container.appendChild
@@ -90,3 +106,24 @@ activeImage.setAttribute('alt', 'First slide');
     <span class="sr-only">Next</span>
   </a> */
 
+// fetch('https://api.github.com/users/blangwwll')
+// .then(response => {
+//   console.log(response);
+//   if (response.status === 200) {
+//     return response.json();
+//   }
+//   })
+
+//   .then(data => {
+//     console.log(data);
+//     if (data.blog === null) {
+//       console.log('there is no info here');
+//     }
+    
+//     if (data.email === null) {
+//       console.log('there is no email here');
+//     }
+// })
+//   .catch(error => {
+//     console.log('this is an error: ' + error);
+//   }) // write after last .then - if something wrong
