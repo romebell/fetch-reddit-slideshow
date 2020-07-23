4b6d21fe989039ceaedf0c4a0235f3351d87b1f3
Chamon562 <Chamon562@gmail.com>
2020-07-23 00:29:12 -0700

    [Character profiles] Add 3 marvel and 3 capcom characters

--- app.js
+++ app.js
@@ -1,5 +1,110 @@
 console.log('Please Tell me this work')
 const container = document.querySelector('.container')
 console.log(container)
-fetch('https://www.reddit.com/search.json?q=rap+nsfw:no')
+const searchBar = document.getElementById('.inputSearch')
+console.log('search bar input ' + searchBar)
 
+fetch('https://secure-hamlet-19722.herokuapp.com/api/v1/characters')
+
+
+.then(response =>{
+    console.log(response)
+    return response.json()
+})
+
+.then(data =>{
+    console.log(data)
+    
+    //Black Heart Profile
+    let bHeartPofile = data[0]
+    console.log(bHeartPofile)
+    let bHeartName = data[0].name
+    console.log(bHeartName)
+    let bHeartPic = data[0].head_shot
+    console.log(bHeartPic)
+    let bHeartSide = data[0].universe
+    console.log(bHeartSide)
+    
+    //WarMachine Proifle
+    let wMachinePofile = data[25]
+    console.log(wMachinePofile)
+    wMachineName = data[25].name
+    console.log(wMachineName)
+    wMachinePic = data[25].head_shot
+    console.log(wMachinePic)
+    wMachineSide = data[25].universe
+    console.log(wMachineSide)
+
+    //Cyclops Profile
+    let CyclopsPofile = data[4]
+    console.log(CyclopsPofile)
+    let CyclopsName = data[4].name
+    console.log(CyclopsName)
+    let CyclopsPic = data[4].head_shot
+    console.log(CyclopsPic)
+    let CyclopsSide = data[4].universe
+    console.log(CyclopsSide)
+    
+    //MegaMan Profile
+    let megaManPofile = data[45]
+    console.log(megaManPofile)
+    let megaManName = data[45].name
+    console.log(megaManName)
+    let megaManPic = data[45].head_shot
+    console.log(megaManPic)
+    let megaManSide = data[45].universe
+    console.log(megaManSide)
+    
+    //Ryu Profile
+    let ryuPofile = data[49]
+    console.log(ryuPofile)
+    let ryuName = data[49].name
+    console.log(ryuName)
+    let ryuPic = data[49].head_shot
+    console.log(ryuPic)
+    let ryuSide = data[49].universe
+    console.log(ryuSide)
+    
+    //Akuma Profile
+    let akumaPofile = data[28]
+    console.log(akumaPofile)
+    let akumaName = data[28].name
+    console.log(akumaName)
+    let akumaPic = data[28].head_shot
+    console.log(akumaPic)
+    let akumaSide = data[28].universe
+    console.log(akumaSide)
+})
+
+// var imgArray = new Array();
+
+// imgArray[0] = new Image();
+// imgArray[0].src = 'images/img/Splash_image1.jpg';
+
+// imgArray[1] = new Image();
+// imgArray[1].src = 'images/img/Splash_image2.jpg';
+
+// /* ... more images ... */
+
+// imgArray[5] = new Image();
+// imgArray[5].src = 'images/img/Splash_image6.jpg';
+
+// /*------------------------------------*/
+
+// function nextImage(element)
+// {
+//     var img = document.getElementById(element);
+
+//     for(var i = 0; i < imgArray.length;i++)
+//     {
+//         if(imgArray[i].src == img.src) // << check this
+//         {
+//             if(i === imgArray.length){
+//                 document.getElementById(element).src = imgArray[0].src;
+//                 break;
+//             }
+//             document.getElementById(element).src = imgArray[i+1].src;
+//             break;
+//         }
+//     }
+// }
