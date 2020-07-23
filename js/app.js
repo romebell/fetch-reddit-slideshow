680021d29436e272e06a0b7916e3394dd8cdb6ac
Martin Briceno <martin.briceno@live.com>
2020-07-23 14:05:03 -0400

    working slide show

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 var container1 = document.getElementById('container1');
 var container2 = document.getElementById('container2');
+var image1 = document.getElementById('image1');
 var stop1 = document.getElementById('stop1');
 var firstPart = 'https://www.reddit.com/search.json?q=';
 var lastPart = '+nsfw:no';
@@ -15,8 +16,41 @@ function submitOne() {
     container2.style.display='block';
     var weSearch = document.getElementById('search1').value;
     var fullAddress = firstPart + weSearch + lastPart;
-    console.log(fullAddress);
-}
+    fetch(fullAddress)
+    .then (response => {
+        return response.json();
+    })
+    .then(data => {
+        // console.log(data.data);
+        // console.log(fullAddress);
+        // console.log(data.data.children[3].data.thumbnail);
+
+        var numb1 = 1;
+
+        imgRot();
+        function imgRot() {
+            if (numb1 > 5) {
+
+                console.log(numb1);
+                numb1 = numb1 - 5;
+
+            } else {
+            if (data.data.children[numb1].data.thumbnail === null) {
+                numb ++;
+            } else {
+
+            image1.src = data.data.children[numb1].data.thumbnail;
+            console.log(data.data.children[numb1].data.thumbnail);
+
+            numb1 ++;
+            setTimeout(imgRot, 2000);
+            }
+        }
+            }
+        })
+    }
+
+
 
 function stopOne () {
     container1.style.display='block';
