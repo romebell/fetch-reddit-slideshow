a717a034719dd6d183c0a702d601104daf182198
Cristina Nguyen <cristinanguyen@MacBook-Pro.local>
2020-07-22 22:19:55 -0700

    [API] loop through API to get random article

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
-fetch("https://www.reddit.com/r/todayilearned/top.json?count=N&after=t3_XXXXX")
+fetch("http://api.reddit.com/hot.json")
+//("https://www.reddit.com/search.json?q=$%7Bvalue%7D+nsfw:no")
+//("https://www.reddit.com/r/todayilearned/top.json?count=N&after=t3_XXXXX")
 .then(response => {
     return response.json();
 })
 .then (data => {
     console.log(data);
+    let allArticles = data.data.children;
+    console.log(allArticles);
+
+    //I'm trying to loop through the articles and return each of them
+    for (i = 0; i < allArticles.length; i++) {
+        const loop = allArticles[i];
+        console.log(loop);
+        return loop
+    }
 })
