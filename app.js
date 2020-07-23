4e7fb4b5fe7f2eb501f501aedc835bd7bfd37e8b
Cristina Nguyen <cristinanguyen@MacBook-Pro.local>
2020-07-23 02:22:37 -0700

    [search button] start function that searches for input value

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function (){
     fetch("https://www.reddit.com/search.json?q=cute+puppies")
 .then(responseData => {
-    console.log(responseData);
+    //console.log(responseData);
     return responseData.json();
 })
 .then(jsonData => {
@@ -10,18 +10,32 @@ document.addEventListener("DOMContentLoaded", function (){
     //let allImages = jsonData.data.children;
     //console.log(allImages);
     let picture = jsonData.data.children;
-    for (let i = 0; i < picture.length; i++) {
+    for (let i = 0; i < 25; i++) {
         //let allImages = jsonData.data.children[i];
         let thumbnail = picture[i].data.url;
-        console.log(thumbnail);
 
+        //console.log(thumbnail);
+
+
+        setInterval(
+
+        function appendImagetoBody() {
+        setInterval(2000);
         const image = document.createElement("img");
         image.src = thumbnail;
-        image.classList.add("image");
-        let body = document.querySelector("body");
-        body.appendChild(image);
-       //console.log(imageCycle);
+        image.classList.add("images");
+        let div = document.querySelector("div");
+        div.appendChild(image);
+        console.log(image)
+    } ,2000);
+        
+    
     }
 })
+
+function convertText() {
+    let grabText = document.querySelector("input").value;
+
+}
 
 })
