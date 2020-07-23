40dbbe00cdb8128d3c3d2abe5ce8f44c4304bfc7
kinawy <sameh.yasser.kinawy@gmail.com>
2020-07-23 14:04:38 -0400

    [JS] Sets Interval finally, thank you Pete, figuring out stopping count

--- js/app.js
+++ js/app.js
@@ -2,87 +2,116 @@ const link = 'http://www.reddit.com/search.json?q=';
 const body = document.querySelector('body');
 let button1 = document.querySelector('i');
 let resetButton = document.querySelector('button');
-
+let count = 0;
+let picArray = [];
 let head1 = document.getElementById('header');
 let para = document.getElementById('description');
 let form = document.querySelector('form');
 let input2 = document.querySelector('input');
+let image1 = document.querySelector('img');
 
 
 
-
-
-
-
-function getTextValue (inputs) {
-
+document.addEventListener('DOMContentLoaded', function(){
+    
     resetButton.addEventListener('click', event => {
-      location.reload()
-
+        location.reload()
+  
     })
-
-    button1.addEventListener('click', event => {
-        
-        resetButton.style.visibility = 'visible'
-        
-        let textValue = document.querySelector('#input').value;
-        let image1 = document.createElement('img');
-        image1.style.width = '225px'
-        
-        console.log(image1);
-        
-        
-
-        body.removeChild(head1);
-        body.removeChild(para);
-        body.removeChild(form);
+    button1.addEventListener('click', getTextValue)
+})
 
 
-        
 
-        
-        let count = 0;
-        
 
-        function makeImage (data) {
-            
-            
-            let loopArray = data.data.children;
-            console.log(loopArray)
-            for (let i = 0; i < 25; i ++) {
-                let pic = loopArray[i].data.url;
-                
-                
-                console.log(pic)
-                if (pic.includes('jpg')) {
-                    
-                    
-                    image1.src = pic
-                    image1.style.height = '500px';
-                    image1.style.width = '500px';
-                    image1.style.borderRadius = '10px';
-                    
-                    body.appendChild(image1);
-                
-                } 
-                
-                
-                
-            }
-            // count ++;
-            // if (count > image1.length) {count = 1}
+
+
+function getTextValue () {
+
+    event.preventDefault();
+    resetButton.style.visibility = 'visible'
         
+    let textValue = document.querySelector('#input').value;
+    
+    body.removeChild(head1);
+    body.removeChild(para);
+    body.removeChild(form);
+    let splitText = textValue.split(' ').join('+');
+    let link = 'http://www.reddit.com/search.json?q=';
+    let input = link + splitText;
+    console.log(input)
+
+
+    fetch(input)
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+
+        makeImage(data)
+    })
+    .catch(error => {
+        console.log('Error', error)
+    })
             
+
+
+
             
+
+
+}
+
+function makeImage (data) {
+
+    let loopArray = data.data.children;
+    
+    setInterval(function () {     
+    
+    
+    
+    console.log(count)
+    image1.src = picArray[count]
+    image1.style.height = '500px';
+    image1.style.width = '500px';
+    image1.style.borderRadius = '10px';
+    count++;
+
+    if (count>picArray.length) {
+        
+    }
+    
+    }, 5000)
+    // if (count === picArray.length) {
+    //     count = 0;
+    // }
+
+    
+
+    for (let i = 0; i < loopArray.length; i ++) {
+        let pic = loopArray[i].data.url;
+        if (pic.includes('jpg')){
+            picArray.push(pic);
             
-            
-                
-                
-                
-            
+            console.log(picArray)
         }
+    }
+    // setInterval(makeImage, 5000)
+} 
 
-                // image1.src = pic;
+
+function slideShow () {
+    ;
+    
+}
+    
+    // count ++;
+    // if (count > image1.length) {count = 1}
+
+
+// getTextValue();
+
+// image1.src = pic;
                 // image1.style.height = '300px';
                 // image1.style.width = '200px';
                 // image1.style.visibility = 'visible';
@@ -99,61 +128,8 @@ function getTextValue (inputs) {
                 //     
                     
                 // }
-            
-        
-                
-        
-        
-        
-            
-        
-        
-
-
-
-
-            
-            
-
-            let splitText = textValue.split(' ').join('+');
-            let link = 'http://www.reddit.com/search.json?q=';
-            let input = link + splitText;
-            console.log(input)
-
-
-            fetch(input)
-            .then(response => {
-                return response.json();
-            })
-            .then(data => {
-
-                makeImage(data)
-            })
-            .catch(error => {
-                console.log('Error', error)
-            })
-            
-
-
-
-            
-
-})
-}
-
-// getTextValue();
 
 
 
 
 
-
-document.addEventListener('DOMContentLoaded', function(){
-
-    
-
-    getTextValue();
-
-
-
-})
