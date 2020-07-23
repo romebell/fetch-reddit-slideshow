2c10e836e877f727bf4a2fea1ed7aa90e2310258
Cristina Nguyen <cristinanguyen@MacBook-Pro.local>
2020-07-23 09:51:09 -0700

    [slideshow loop] refactor loop - images looping through screen too fast- only shows last image

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 document.addEventListener("DOMContentLoaded", function (){
     fetch("https://www.reddit.com/search.json?q=cute+puppies")
 .then(responseData => {
@@ -10,32 +11,37 @@ document.addEventListener("DOMContentLoaded", function (){
     //let allImages = jsonData.data.children;
     //console.log(allImages);
     let picture = jsonData.data.children;
-    for (let i = 0; i < 25; i++) {
-        //let allImages = jsonData.data.children[i];
+    //const image = document.createElement("img");
+
+    for (let i = 0; i < 20; i++) {
         let thumbnail = picture[i].data.url;
-
         //console.log(thumbnail);
-
-
-        setInterval(
-
+        setInterval (
         function appendImagetoBody() {
-        setInterval(2000);
+        //setInterval(2000);
+
+        const previousImg = document.querySelector(".images");
+        //previousImg.remove();
+        if (previousImg != null) {
+            previousImg.remove();
+        }
+
         const image = document.createElement("img");
         image.src = thumbnail;
         image.classList.add("images");
         let div = document.querySelector("div");
         div.appendChild(image);
-        console.log(image)
-    } ,2000);
-        
-    
-    }
-})
+        //console.log(image)
+    } , 2000);
+}
+}) 
 
-function convertText() {
+//function to search for images when button is pressed
+function searchReddit() {
     let grabText = document.querySelector("input").value;
+    let searchButton = document.querySelector("search");
 
 }
 
 })
+
