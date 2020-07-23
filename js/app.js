cc6f36e05413427c20106fe31c084c8410012fdf
kinawy <sameh.yasser.kinawy@gmail.com>
2020-07-22 21:25:26 -0400

    [JS/FETCH] creates fetch, loops through thumbnails, creates new image element, still working

--- js/app.js
+++ js/app.js
@@ -1,11 +1,54 @@
-fetch('http://www.reddit.com/search.json?q=')
-then.(response => {
-    return response.json();
-})
-then.(data => {
-
-    console.log(data)
+const link = 'http://www.reddit.com/search.json?q=cats';
+const body = document.querySelector('body');
+let button = document.querySelector('i');
+let textValue = document.querySelector('#input').value;
 
 
+function getTextValue () {
+    
+    button.addEventListener('click', event => {
+        let textValue = document.querySelector('#input').value;
+        console.log(textValue);
+    })
+}
+getTextValue();
+
+
+
+
+document.addEventListener('DOMContentLoaded', function(){
+
+    fetch(link)
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+
+        let loopArray = data.data.children;
+        console.log(loopArray);
+
+        for (let i = 0; i < 25; i ++) {
+            
+            let pic = loopArray[i].data.thumbnail;
+            console.log(pic);
+            let newImage = document.createElement('img');
+            
+            if (pic === 'self') {
+                newImage.src = 'https://picsum.com/200/300';
+                setTimeout(body.appendChild(newImage), 5000)
+            } else {
+                newImage.src = pic;
+                body.appendChild(newImage)
+            }
+
+            
+
+
+
+        }
+
+
+
+    })
 
 })
