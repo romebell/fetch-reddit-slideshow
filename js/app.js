5082285fe725959a3c6ab4abcee08d7c73258e81
margaret-jihua <margaret.jihua@gmail.com>
2020-07-23 00:32:33 -0700

    [css] Adjust picture slide position

--- js/app.js
+++ js/app.js
@@ -4,51 +4,52 @@ const submit = document.querySelector('.search-btn')
 
 submit.addEventListener('click', function() {
     searchBar.style.display = 'none'
-    console.log("searching");
     const input = document.querySelector('.search-input').value
     console.log(input);
-})
 
-fetch('https://www.reddit.com/search.json?q=cats+nsfw:no')
-.then(response => {
-    return response.json()
-})
-.then(jsonData => {
-    let data = jsonData.data
-    let children = data.children
-    let pictureUrls = []
-    children.forEach(eachChild => {
-        let childData = eachChild.data
-        let pictureUrl = childData.thumbnail
-        if (pictureUrl !== 'self'){
-            pictureUrls.push(pictureUrl)
-        }        
+    //need to do somethinf with input 
+    fetch('https://www.reddit.com/search.json?q=cats+nsfw:no')
+    .then(response => {
+        return response.json()
     })
-    // console.log(pictureUrls) 
-    
-    // Add Carousel from Bootstrap
+    .then(jsonData => {
+        let data = jsonData.data
+        let children = data.children
+        let pictureUrls = []
+        children.forEach(eachChild => {
+            let childData = eachChild.data
+            let pictureUrl = childData.thumbnail
+            if (pictureUrl !== 'self'){
+                pictureUrls.push(pictureUrl)
+            }        
+        })
+        
+        // Add Carousel from Bootstrap
 
-    const carousel = document.createElement('div')
-    carousel.setAttribute('id', 'carouselExampleSlidesOnly')
-    carousel.setAttribute('class', 'carousel slide')
-    carousel.setAttribute('data-ride', 'carousel')
+        const carousel = document.createElement('div')
+        carousel.setAttribute('id', 'carouselExampleSlidesOnly')
+        carousel.setAttribute('class', 'carousel slide')
+        carousel.setAttribute('data-ride', 'carousel')
 
-    const carouselInner = document.createElement('div')
-    carouselInner.classList.add('carousel-inner')
-    
-    const carouselItem = document.createElement('div')
-    carouselItem.classList.add('carousel-item')
-    carouselItem.classList.add('active')
+        const carouselInner = document.createElement('div')
+        carouselInner.classList.add('carousel-inner')
+        
+        const carouselItem = document.createElement('div')
+        carouselItem.classList.add('carousel-item')
+        carouselItem.classList.add('active')
 
-    const image = document.createElement('img')
-    image.src = pictureUrls[0]
-    image.setAttribute('class', 'd-block w-100')
+        const image = document.createElement('img')
+        image.src = pictureUrls[0]
+        image.setAttribute('class', 'd-block w-100')
 
-    carouselItem.appendChild(image)
-    carouselInner.appendChild(carouselItem)
-    carousel.appendChild(carouselInner)
+        carouselItem.appendChild(image)
+        carouselInner.appendChild(carouselItem)
+        carousel.appendChild(carouselInner)
+        body.appendChild(carousel)
+        console.log(body);
+    })
+})
 
-    console.log(carousel);
     
     //console.log(carousel, carouselInner, carouselItem, image);
 
@@ -67,5 +68,5 @@ fetch('https://www.reddit.com/search.json?q=cats+nsfw:no')
 // </div>
 
 //.carousel('pause')
-})
+
 
