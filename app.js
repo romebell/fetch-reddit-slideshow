bae71f360522f6468e2bd735e795283728d2651c
Philip Yap <philipyap@Philips-MacBook.local>
2020-07-23 11:07:31 -0700

    [fetch push]

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 
-const container = document.querySelector('.container')
+//document.addEventListener("DOMContentLoaded", function(){
+//const container = document.querySelector('.container')
 const stop = document.querySelector('.stop')
 
 
@@ -9,19 +10,31 @@ fetch ('https://www.reddit.com/search.json?q=famous+buildings+nsfw:no')
     console.log(response)
     return response.json();
 
-
+   
 })
-
 .then (data => {
 
     console.log(data);
-    let thumbnailOne = data.data.children[0].data.thumbnail;
+
+    let thumbnailOne =data.data.children[0].data.thumbnail;
     console.log(thumbnailOne);
     let thumbnailTwo = data.data.children[1].data.thumbnail;
     console.log(thumbnailTwo);
     let thumbnailThree = data.data.children[2].data.thumbnail;
     console.log(thumbnailThree);
     
+    // let thumbnail = children.map(function(thumbnailResults){
+    // let image = thumbnailResults.data.thumbnail;
+    // return image;
+    })
+    
+    
+
+const imageShow = document.createElement('div')
+thumbnail.forEach(image => {
+    let imageBuilding = document.createElement('image');
+    imageBuilding.src = `${image}`;
+    imageShow.appendChild(imageBuilding);
 })
 
 const getStop = () => {
@@ -29,3 +42,5 @@ const getStop = () => {
 stop.addEventListener('click', getStop)
 
 }
+
+
