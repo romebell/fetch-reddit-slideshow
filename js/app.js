c30c4d14d32832a495d33c301575f60c87601cd9
Martin Briceno <martin.briceno@live.com>
2020-07-23 10:51:39 -0400

    both containers dissapear and appear at click of button

--- js/app.js
+++ js/app.js
@@ -1,13 +1,25 @@
+var container1 = document.getElementById('container1');
+var container2 = document.getElementById('container2');
+var stop1 = document.getElementById('stop1');
 var firstPart = 'https://www.reddit.com/search.json?q=';
-var lastPart = '+nsfw:no'
-var button1 = document.getElementById('submit1');
-var field1 = document.getElementById('search1');
-document.getElementById('button1').addEventListener('click', submitOne);
-var weSearch = submitOne();
+var lastPart = '+nsfw:no';
+var submit1 = document.getElementById('submit1');
+var search1 = document.getElementById('search1');
+document.getElementById('submit1').addEventListener('click', submitOne);
+document.getElementById('stop1').addEventListener('click', stopOne);
+var weSearch = 'cats';
 var fullAddress = firstPart + weSearch + lastPart;
 
 function submitOne() {
-
-
+    // document.getElementById("container1").display = "none";
+    // document.getElementById("container2").display = "block";
+    container1.style.display='none';
+    container2.style.display='block';
+    console.log(fullAddress);
+  //  return field1.nodeValue
 }
-//https://www.reddit.com/search.json?q=cats+nsfw:no
+
+function stopOne () {
+    container1.style.display='block';
+    container2.style.display='none';
+}
